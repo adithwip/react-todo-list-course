3e727043bb8a4cbd47866066e149010677f4e3a6
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2019-12-23 08:58:43 +0700

    add Header component and implements it to index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 import Paper from "./components/Paper";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn main-black-color">Add</button>
-        <h1 className="header-title">Todo Lists</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
+      <Header />
       <section className="add">
         <form className="add-form">
           <input className="add-input" type="text" />
