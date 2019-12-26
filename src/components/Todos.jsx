e9895b9ba4000a18d5594acfc30e756e87c33969
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2019-12-26 11:32:36 +0700

    Wrap all todo related component to todolist page component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,38 +1,45 @@
 import React from "react";
 
+import Todo from "./Todo";
+
 const Todos = () => {
+  const todos = [
+    {
+      text: "Learning React!"
+    },
+    {
+      text: "Learning Props in React"
+    },
+    {
+      text: "Learning React Hooks!"
+    },
+    {
+      text: "Learning React!"
+    },
+    {
+      text: "Learning Props in React"
+    },
+    {
+      text: "Learning React Hooks!"
+    },
+    {
+      text: "Learning Props in React"
+    },
+    {
+      text: "Learning React Hooks!"
+    },
+    {
+      text: "Learning Props in React"
+    },
+    {
+      text: "Learning React Hooks!"
+    }
+  ];
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
+      {todos.map(todo => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
