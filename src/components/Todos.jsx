5e4e220f792a2c3686e97b452b5baa3546c1a7e7
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2019-12-26 13:36:48 +0700

    process todos value injected from parent in Todos component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,39 +2,7 @@ import React from "react";
 
 import Todo from "./Todo";
 
-const Todos = () => {
-  const todos = [
-    {
-      text: "Learning React!"
-    },
-    {
-      text: "Learning Props in React"
-    },
-    {
-      text: "Learning React Hooks!"
-    },
-    {
-      text: "Learning React!"
-    },
-    {
-      text: "Learning Props in React"
-    },
-    {
-      text: "Learning React Hooks!"
-    },
-    {
-      text: "Learning Props in React"
-    },
-    {
-      text: "Learning React Hooks!"
-    },
-    {
-      text: "Learning Props in React"
-    },
-    {
-      text: "Learning React Hooks!"
-    }
-  ];
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map(todo => {
